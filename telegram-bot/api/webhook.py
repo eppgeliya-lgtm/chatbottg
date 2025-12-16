03ffbff92b38165e4d85801f02733f8f2f7ad597
eppgeliya-lgtm <eppgeliya@gmail.com>
2025-12-16 15:15:51 +0300

    Refactor webhook function to remove error handling
    Removed try-except block from webhook function.

--- telegram-bot/api/webhook.py
+++ telegram-bot/api/webhook.py
@@ -1,22 +1,18 @@
 import os
 from fastapi import FastAPI, Request
-from bot import bot, dp  # импортируем bot и dp из bot.py
+from bot import bot, dp
 from aiogram.types import Update
 
 app = FastAPI()
 
-# POST-запрос для Telegram
 @app.post("/webhook")
 async def webhook(request: Request):
-    try:
-        data = await request.json()
-        update = Update.model_validate(data)
-        await dp.feed_update(bot, update)
-        return {"ok": True}
-    except Exception as e:
-        return {"ok": False, "error": str(e)}
+    data = await request.json()
+    update = Update.model_validate(data)
+    await dp.feed_update(bot, update)
+    return {"ok": True}
 
-# GET-запрос для проверки, что webhook доступен
 @app.get("/webhook")
 async def check():
     return {"ok": True, "message": "Webhook is alive"}
+

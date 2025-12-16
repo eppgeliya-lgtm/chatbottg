5a88f23eade3e674341c834f3b6c7d0b96859980
eppgeliya-lgtm <eppgeliya@gmail.com>
2025-12-16 14:23:57 +0300

    Update webhook.py

--- telegram-bot/api/webhook.py
+++ telegram-bot/api/webhook.py
@@ -1,7 +1,7 @@
 import os
+from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
 from aiogram.types import Update
-from fastapi import FastAPI, Request
 
 BOT_TOKEN = os.environ.get("8525182861:AAEZlNNf0m1U_KS7ZcuB9WksJkGlvvmflfM")
 bot = Bot(token=BOT_TOKEN)
@@ -9,13 +9,13 @@ dp = Dispatcher()
 app = FastAPI()
 
 @app.post("/webhook")
-async def handler(request: Request):
+async def webhook(request: Request):
     data = await request.json()
     update = Update.model_validate(data)
-    await dp.feed_update(bot, Update.model_validate(data))
+    await dp.feed_update(bot, update)
     return {"ok": True}
 
-# Для проверки через браузер
 @app.get("/webhook")
 async def check():
     return {"ok": True, "message": "Webhook is alive"}
+
